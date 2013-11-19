ce8ce81f0c1956faa1cdf7e29d4a8756f79f495d
rgalanakis <rob.galanakis@gmail.com>
2013-11-19 15:06:01 +0000

    Fixes

--- src/chapter2/charcreator.py
+++ src/chapter2/charcreator.py
@@ -5,7 +5,7 @@ def convert_hierarchies_main():
     """`convert_hierarchies(pmc.selection())`.
     Prints and provides user feedback so only call from UI.
     """
-    nodes = pmc.selection(type='transform') #(1)
+    nodes = pmc.selected(type='transform') #(1)
     if not nodes:
         pmc.warning('No transforms selected.') #(2)
         return
@@ -19,8 +19,8 @@ def convert_hierarchies(rootnodes):
     hierarchy assuming `child` lives under it).
     """
     roots = skeletonutils.uniqueroots(rootnodes)
-    for r in roots:
-        convert_hierarchy(r)
+    result = [convert_hierarchy(r) for r in roots]
+    return result
 
 
 def convert_hierarchy(node):
@@ -30,12 +30,10 @@ def convert_hierarchy(node):
     Connections to nodes are not preserved on the newly
     created joints.
     """
-    skeletonutils.convert_to_skeleton(node)
+    result = skeletonutils.convert_to_skeleton(node)
     pmc.delete(node)
+    return result
 
-
-import pymel.core as pmc
-import skeletonutils
 
 # (1)
 SETTINGS_DEFAULT = {
@@ -53,28 +51,29 @@ SETTINGS_GAME2 = {
     'prefix': 'gamechar_',
 }
 
-# (2)
-def convert_hierarchy(rootnode,  settings=SETTINGS_DEFAULT):
-    skeletonutils.convert_to_skeleton( #(3)
-        node,
-         jointsize=settings['joint_size'],
-        prefix=settings['prefix'],
-        rcol=settings['right_color'],
-        lcol=settings['left_color'],
-        ccol=settings['center_color'])
-    pmc.delete(node)
-
-#(2)
-def convert_hierarchies(rootnodes, settings=SETTINGS_DEFAULT):
-    roots = skeletonutils.trueroots(rootnodes)
-    for r in roots:
-        convert_hierarchy(r, settings)
-
 #(2)
 def convert_hierarchies_main(settings=SETTINGS_DEFAULT):
-    nodes = pmc.selection(type='transform')
+    nodes = pmc.selected(type='transform')
     if not nodes:
         pmc.warning('No transforms selected.')
         return
     new_roots = convert_hierarchies(nodes, settings)
     print 'Created:', ','.join([r.name() for r in new_roots])
+
+#(2)
+def convert_hierarchies(rootnodes, settings=SETTINGS_DEFAULT):
+    roots = skeletonutils.uniqueroots(rootnodes)
+    result = [convert_hierarchy(r, settings) for r in roots]
+    return result
+
+# (2)
+def convert_hierarchy(rootnode,  settings=SETTINGS_DEFAULT):
+    result = skeletonutils.convert_to_skeleton( #(3)
+        rootnode,
+        jointsize=settings['joint_size'],
+        prefix=settings['prefix'],
+        rcol=settings['right_color'],
+        lcol=settings['left_color'],
+        ccol=settings['center_color'])
+    pmc.delete(rootnode)
+    return result
