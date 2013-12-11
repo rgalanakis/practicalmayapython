4fbdb569f06720f4bdc25833ab74747c6caedf52
rgalanakis <rob.galanakis@gmail.com>
2013-12-11 12:22:28 +0000

    Includes runnable demo code

--- src/chapter5/hierarchyconvertergui.py
+++ src/chapter5/hierarchyconvertergui.py
@@ -1,209 +1,244 @@
 from PySide import QtGui, QtCore
 
-def create_window():
-    win = QtGui.QMainWindow()
-    return win
+
+def version1():
+    """Very basic first version."""
+    def create_window():
+        win = QtGui.QMainWindow()
+        return win
+
+    if __name__ == '__main__':
+        app = QtGui.QApplication([])
+        win = create_window()
+        win.show()
+        app.exec_()
+#version1()
 
 
-if __name__ == '__main__':
-    app = QtGui.QApplication([])
-    win = create_window()
-    win.show()
-    app.exec_()
+def version2():
+    """Shows creating textbox, layout, and central widget."""
+    def create_window():
+        # Create the widgets, starting with a window
+        window = QtGui.QMainWindow()
+        # Create a container that is parented to the window
+        container = QtGui.QWidget(window)
+        # Create a textbox that is parented to the container
+        textbox = QtGui.QLineEdit(container)
 
-# Version 2
+        # Now, set up the layout.
+        layout = QtGui.QHBoxLayout(container)
+        container.setLayout(layout)
+        layout.addWidget(textbox)
+        window.setCentralWidget(container)
 
-def create_window():
-    # Create the widgets, starting with a window
-    window = QtGui.QMainWindow()
-    # Create a container that is parented to the window
-    container = QtGui.QWidget(window)
-    # Create a textbox that is parented to the container
-    textbox = QtGui.QTextBox(container)
+        # Return the window so it can be shown
+        return window
 
-    # Now, set up the layout.
-    layout = QtGui.QHBoxLayout(container)
-    container.setLayout(layout)
-    layout.addWidget(textbox)
+    if __name__ == '__main__':
+        app = QtGui.QApplication([])
+        win = create_window()
+        win.show()
+        app.exec_()
+#version2()
 
+def version3():
+    """Adds window title and multiple widgets."""
 
-    # Return the window so it can be shown
-    return window
+    def create_window():
+        window = QtGui.QMainWindow()
+        window.setWindowTitle('Hierarchy Converter')
 
+        container = QtGui.QWidget(window)
+        label = QtGui.QLabel('Prefix:', container)
+        textbox = QtGui.QLineEdit(container)
+        button = QtGui.QPushButton('Convert', container)
 
-# Version 3
+        layout = QtGui.QHBoxLayout(container)
+        container.setLayout(layout)
+        # Add them to the layout, first widget added is left-most
+        layout.addWidget(label)
+        layout.addWidget(textbox)
+        layout.addWidget(button)
+        window.setCentralWidget(container)
 
-def create_window():
-    window = QtGui.QMainWindow()
-    # Always set the window title to something meaningful!
-    window.setTitle("Skeleton Converter")
-    container = QtGui.QWidget(window)
+        return window
 
-    label = QtGui.QLabel("Prefix:", container)
-    textbox = QtGui.QTextBox(container)
-    button = QtGui.QPushButton("Convert", container)
-
-    layout = QtGui.QHBoxLayout(container)
-    container.setLayout(layout)
-    # Add them to the layout, first widget added is left-most
-    layout.addWidget(label)
-    layout.addWidget(textbox)
-    layout.addWidget(button)
-
-    return window
-
-
-# Version 4
-
-def create_window():
-    window = QtGui.QMainWindow()
-    # Always set the window title to something meaningful!
-    window.setTitle("Skeleton Converter")
-    container = QtGui.QWidget(window)
-
-    label = QtGui.QLabel("Prefix:", container)
-    textbox = QtGui.QTextBox(container)
-    button = QtGui.QPushButton("Convert", container)
-
-    window.convertClicked = QtCore.Signal(str)
-    def onclick():
-        window.convertClicked.emit(textbox.text())
-    button.clicked.connect(onclick)
-
-    layout = QtGui.QHBoxLayout(container)
-    container.setLayout(layout)
-    # Add them to the layout, first widget added is left-most
-    layout.addWidget(label)
-    layout.addWidget(textbox)
-    layout.addWidget(button)
-
-    return window
-
-if __name__ == '__main__':
-    def onconvert(prefix):
-        print 'Convert clicked! Prefix:', prefix
-    app = QtGui.QApplication([])
-    win = create_window()
-    win.convertClicked.connect(onconvert)
-    win.show()
-    app.exec_()
+    if __name__ == '__main__':
+        app = QtGui.QApplication([])
+        win = create_window()
+        win.show()
+        app.exec_()
+#version3()
 
 
-# Version 5
+def version4():
+    """Adds convertClicked signal and support."""
 
-class HierarchyConverterController(object):
-    selectionChanged = QtCore.Signal()
+    class ConverterWindow(QtGui.QMainWindow):
+        convertClicked = QtCore.Signal(str)
 
-def create_window(controller):
-    window = QtGui.QMainWindow()
-    # Always set the window title to something meaningful!
-    window.setTitle("Skeleton Converter")
-    container = QtGui.QWidget(window)
+    def create_window():
+        window = ConverterWindow()
+        window.setWindowTitle('Hierarchy Converter')
 
-    label = QtGui.QLabel("Prefix:", container)
-    textbox = QtGui.QTextBox(container)
-    button = QtGui.QPushButton("Convert", container)
+        container = QtGui.QWidget(window)
+        label = QtGui.QLabel('Prefix:', container)
+        textbox = QtGui.QLineEdit(container)
+        button = QtGui.QPushButton('Convert', container)
 
-    window.convertClicked = QtCore.Signal(str)
-    def onclick():
-        window.convertClicked.emit(textbox.text())
-    button.clicked.connect(onclick)
+        def onclick():
+            window.convertClicked.emit(textbox.text())
+        button.clicked.connect(onclick)
 
-    def onSelChanged(newsel):
-        sb = window.statusBar()
-        if not newsel:
-            txt = 'Nothing selected.'
-        elif len(newsel) == 1:
-            txt = '%s selected.' % newsel[0]
-        else:
-            txt = '%s objects selected.' % len(newsel)
-        sb.setText(txt)
-    controller.selectionChanged.connect(onSelChanged)
+        layout = QtGui.QHBoxLayout(container)
+        container.setLayout(layout)
+        layout.addWidget(label)
+        layout.addWidget(textbox)
+        layout.addWidget(button)
+        window.setCentralWidget(container)
 
-    layout = QtGui.QHBoxLayout(container)
-    container.setLayout(layout)
-    # Add them to the layout, first widget added is left-most
-    layout.addWidget(label)
-    layout.addWidget(textbox)
-    layout.addWidget(button)
+        return window
 
-    return window
+    if __name__ == '__main__':
+        def onconvert(prefix):
+            print 'Convert clicked! Prefix:', prefix
+        app = QtGui.QApplication([])
+        win = create_window()
+        win.convertClicked.connect(onconvert)
+        win.show()
+        app.exec_()
+#version4()
 
-if __name__ == '__main__':
-    def onconvert():
-        print 'Convert clicked!'
-    app = QtGui.QApplication([])
-    controller = HierarchyConverterController()
-    win = create_window(controller)
-    win.convertClicked.connect(onconvert)
-    win.show()
-    app.exec_()
+def version5():
+    """Adds selection changed handling, controller, and status bar."""
 
+    class HierarchyConverterController(QtCore.QObject):
+        selectionChanged = QtCore.Signal(list)
 
-# Version 6
+    class ConverterWindow(QtGui.QMainWindow):
+        convertClicked = QtCore.Signal(str)
 
-import threading
+    def create_window(controller):
+        window = ConverterWindow()
+        window.setWindowTitle('Hierarchy Converter')
+        statusbar = window.statusBar()
 
+        container = QtGui.QWidget(window)
+        label = QtGui.QLabel('Prefix:', container)
+        textbox = QtGui.QLineEdit(container)
+        button = QtGui.QPushButton('Convert', container)
 
-def _pytest():
-    def onconvert():
-        print 'Convert clicked!'
+        def onclick():
+            window.convertClicked.emit(textbox.text())
+        button.clicked.connect(onclick)
 
-    app = QtGui.QApplication([])
+        def onSelChanged(newsel):
+            if not newsel:
+                txt = 'Nothing selected.'
+            elif len(newsel) == 1:
+                txt = '%s selected.' % newsel[0]
+            else:
+                txt = '%s objects selected.' % len(newsel)
+            statusbar.showMessage(txt)
+        controller.selectionChanged.connect(onSelChanged)
 
-    controller = HierarchyConverterController()
-    def nextsel():
-        while True:
-            yield []
-            yield ['single']
-            yield ['single', 'double']
-    getnextsel = nextsel().next
+        layout = QtGui.QHBoxLayout(container)
+        container.setLayout(layout)
+        layout.addWidget(label)
+        layout.addWidget(textbox)
+        layout.addWidget(button)
+        window.setCentralWidget(container)
 
-    def starttimer():
-        controller.selectionChanged.emit(getnextsel())
-        threading.Timer(2, starttimer)
-    starttimer()
+        return window
 
-    win = create_window(controller)
-    win.convertClicked.connect(onconvert)
-    win.show()
-    app.exec_()
+    def _pytest():
+        import random
 
-if __name__ == '__main__':
-    _pytest()
+        controller = HierarchyConverterController()
+        def nextsel():
+            return random.choice([
+                [],
+                ['single'],
+                ['single', 'double']
+            ])
 
+        def onconvert(prefix):
+            print 'Convert clicked! Prefix:', prefix
+            controller.selectionChanged.emit(nextsel())
 
-def create_window(controller, parent=None):
-    window = QtGui.QMainWindow(parent)
-    # Always set the window title to something meaningful!
-    window.setTitle("Skeleton Converter")
-    container = QtGui.QWidget(window)
+        app = QtGui.QApplication([])
+        win = create_window(controller)
+        win.convertClicked.connect(onconvert)
+        win.show()
+        app.exec_()
 
-    label = QtGui.QLabel("Prefix:", container)
-    textbox = QtGui.QTextBox(container)
-    button = QtGui.QPushButton("Convert", container)
+    if __name__ == '__main__':
+        _pytest()
+#version5()
 
-    window.convertClicked = QtCore.Signal(str)
-    def onclick():
-        window.convertClicked.emit(textbox.text())
-    button.clicked.connect(onclick)
+def version6():
+    """Adds parent support."""
 
-    def onSelChanged(newsel):
-        sb = window.statusBar()
-        if not newsel:
-            txt = 'Nothing selected.'
-        elif len(newsel) == 1:
-            txt = '%s selected.' % newsel[0]
-        else:
-            txt = '%s objects selected.' % len(newsel)
-        sb.setText(txt)
-    controller.selectionChanged.connect(onSelChanged)
+    class HierarchyConverterController(QtCore.QObject):
+        selectionChanged = QtCore.Signal(list)
 
-    layout = QtGui.QHBoxLayout(container)
-    container.setLayout(layout)
-    # Add them to the layout, first widget added is left-most
-    layout.addWidget(label)
-    layout.addWidget(textbox)
-    layout.addWidget(button)
+    class ConverterWindow(QtGui.QMainWindow):
+        convertClicked = QtCore.Signal(str)
 
-    return window
+    def create_window(controller, parent=None):
+        window = ConverterWindow(parent)
+        window.setWindowTitle('Hierarchy Converter')
+        statusbar = window.statusBar()
+
+        container = QtGui.QWidget(window)
+        label = QtGui.QLabel('Prefix:', container)
+        textbox = QtGui.QLineEdit(container)
+        button = QtGui.QPushButton('Convert', container)
+
+        def onclick():
+            window.convertClicked.emit(textbox.text())
+        button.clicked.connect(onclick)
+
+        def onSelChanged(newsel):
+            if not newsel:
+                txt = 'Nothing selected.'
+            elif len(newsel) == 1:
+                txt = '%s selected.' % newsel[0]
+            else:
+                txt = '%s objects selected.' % len(newsel)
+            statusbar.showMessage(txt)
+        controller.selectionChanged.connect(onSelChanged)
+
+        layout = QtGui.QHBoxLayout(container)
+        container.setLayout(layout)
+        layout.addWidget(label)
+        layout.addWidget(textbox)
+        layout.addWidget(button)
+        window.setCentralWidget(container)
+
+        return window
+
+    def _pytest():
+        import random
+
+        controller = HierarchyConverterController()
+        def nextsel():
+            return random.choice([
+                [],
+                ['single'],
+                ['single', 'double']
+            ])
+
+        def onconvert(prefix):
+            print 'Convert clicked! Prefix:', prefix
+            controller.selectionChanged.emit(nextsel())
+
+        app = QtGui.QApplication([])
+        win = create_window(controller)
+        win.convertClicked.connect(onconvert)
+        win.show()
+        app.exec_()
+
+    if __name__ == '__main__':
+        _pytest()
+version6()
