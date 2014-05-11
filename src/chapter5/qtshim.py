3a42c61de15fc8e0420ff19fb9f8fc95311d9952
Rob Galanakis <rob.galanakis@gmail.com>
2014-05-11 13:43:54 -0500

    Fixed PySide stuff. It was just totally wrong.

--- src/chapter5/qtshim.py
+++ src/chapter5/qtshim.py
@@ -3,27 +3,34 @@ Provides a common interface between PyQt4 and PySide.
 """
 
 try:
-    from PySide import QtCore, QtGui, shiboken
+    from PySide import QtCore, QtGui
+    import shiboken
     Signal = QtCore.Signal
+
+    def _getcls(name):
+        result = getattr(QtGui, name, None)
+        if result is None:
+            result = getattr(QtCore, name, None)
+        return result
 
     def wrapinstance(ptr):
         """Converts a pointer (int or long) into the concrete
         PyQt/PySide object it represents."""
         # pointers for Qt should always be long integers
         ptr = long(ptr)
-        # Get the QObject for this pointer,
-        # so we can get the class info and the real type eventually.
+        # Get the pointer as a QObject, and use metaObject
+        # to find a better type.
         qobj = shiboken.wrapInstance(ptr, QtCore.QObject)
         metaobj = qobj.metaObject()
+        # Look for the real class in qt namespaces.
+        # If not found, walk up the hierarchy.
+        # When we reach the top of the Qt hierarchy,
+        # we'll break out of the loop since we'll eventually
+        # reach QObject.
         realcls = None
-        # QObject is the base class for all Qt objects,
-        # so we'll get here eventually!
-        while realcls != QtCore.QObject:
-            clsname = metaobj.className()
-            # Look for this class on QtGui or QtCore.
-            realcls = getattr(QtGui, clsname, None)
-            if realcls is None:
-                realcls = getattr(QtCore, clsname, None)
+        while realcls is None:
+            realcls = _getcls(metaobj.className())
+            metaobj = metaobj.superClass()
         # Finally, return the same pointer/object
         # as its most specific type.
         return shiboken.wrapInstance(ptr, realcls)
